56ba047a2134508be7f8768dfddacf3bfa943990
yi <richardzhu00121@outlook.com>
2025-09-20 05:06:35 +0000

    update asv19

--- download/get_asvspoof2019-la.sh
+++ download/get_asvspoof2019-la.sh
@@ -7,7 +7,9 @@ DEST="$ROOT/asvspoof2019"
 mkdir -p "$DEST/raw"
 mkdir -p "$DEST/processed"
 
-wget -c -O $DEST/raw/asvspoof2019.zip https://datashare.ed.ac.uk/download/DS_10283_3336.zip
-unzip -o $DEST/raw/asvspoof2019.zip -d "$DEST/processed"
+# wget -c -O $DEST/raw/asvspoof2019.zip https://datashare.ed.ac.uk/download/DS_10283_3336.zip
+# unzip -o $DEST/raw/asvspoof2019.zip -d "$DEST/raw"
+
+unzip -o $DEST/raw/LA.zip -d $DEST/processed
 
 echo " --- ASVspoof 2019 download complete. File saved in $DEST ---"
